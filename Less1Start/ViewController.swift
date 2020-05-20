1717d463ec8df9c4966cd7b8bc07e48d68a541ee
kramarkirill <kkramar@kantiana.ru>
2020-05-20 14:16:21 +0200

    Initial Commit

---
+++ Less1Start/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Less1Start
+//
+//  Created by Кирилл Крамар on 20.05.2020.
+//  Copyright © 2020 Кирилл Крамар. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
