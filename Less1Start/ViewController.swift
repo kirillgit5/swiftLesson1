d6c4de696cf745064314e6362ee06ec55bad2e0c
kramarkirill <kkramar@kantiana.ru>
2020-05-20 17:26:29 +0200

    Work with IB

--- Less1Start/ViewController.swift
+++ Less1Start/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    
+    @IBOutlet weak var showTextButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showTextButtonPressed() {
+        
+    }
+    
 }
 
